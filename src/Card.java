4a886dfb54b62dd285180e809e023309b47bdbd2
tusharaakoliya <tusharaakoliya001@gmail.com>
2021-01-27 15:35:19 -0500

    Initial commit

---
+++ src/Card.java
@@ -0,0 +1,10 @@
+public class Card {
+    // These are instance variables , private  variables
+    private String faceName, suit;
+    private int faceValue;
+
+    /***
+     * The goal 
+     */
+    /
+}
